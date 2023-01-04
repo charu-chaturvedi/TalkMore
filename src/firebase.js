60635392e831ed8bc4a6ce37b14ad908927efddd
Charu Chaturvedi <charuchaturvedi104@gmail.com>
2023-01-04 16:46:50 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBtcuj5D5tqu6madwPaZqw7Ju8ghaQVNI",
+  authDomain: "talk-more-69440.firebaseapp.com",
+  projectId: "talk-more-69440",
+  storageBucket: "talk-more-69440.appspot.com",
+  messagingSenderId: "764761497753",
+  appId: "1:764761497753:web:cfbbeada885e9c49bf5808"
+};
+
+
+export const app = initializeApp(firebaseConfig);
